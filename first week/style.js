2c3eeac8610926c8105c64bef58753f78968c3df
yangwenqing <furo_yang@163.com>
2018-07-09 15:13:26 +0800

    add side-menu

--- first week/style.js
+++ first week/style.js
@@ -6,25 +6,47 @@ function MouseOverStyle(Obj,str) {
 	menu.style.color = "#fff";
 }
 //鼠标离开菜单栏
-function MouseOutStyle(Obj,str) {
+function MouseOutStyle(Obj,str1,str2) {
 	// body...
-	Obj.style.backgroundColor = "#2F4050";
-	var menu = document.getElementById(str);
-	menu.style.color = "#A7B1C2";
-}
-//鼠标点击一级菜单展开二级菜单或者关闭二级菜单
-function ClickFirstMenu(Obj,str)
-{
-	var sub_menu = document.getElementById(str);
-	var dip_status = sub_menu.style.display;
-	if (dip_status == "none") 
+	var menu = document.getElementById(str1);
+	if (str2 == '') 
 	{
-		Obj.style.height = "210px";
-		Obj.style.backgroundColor = "#293846";
+		menu.style.color = "#A7B1C2";
+		Obj.style.backgroundColor = "#2F4050";
 	}
 	else
 	{
-		Obj.style.height = "70px";
+		//检测二级菜单是否处于展开状态，若展开，鼠标移开样式不变
+		var sub_menu = document.getElementById(str2);
+		var disp_status = sub_menu.style.display;
+		//alert(disp_status);
+		if(disp_status == "none")
+		{
+			menu.style.color = "#A7B1C2";
+			Obj.style.backgroundColor = "#2F4050";
+		}
+	}
+}
+//鼠标点击一级菜单展开二级菜单或者关闭二级菜单
+function ClickFirstMenu(str1,str2)
+{
+	var menu = document.getElementById(str1);
+	var sub_menu = document.getElementById(str2);
+	var dip_status = sub_menu.style.display;
+	//alert("status:"+dip_status);
+	if (dip_status == "none") 
+	{
+		menu.style.height = "200px";
+		menu.style.width = "197px";
+		menu.style.backgroundColor = "#293846";
+		sub_menu.style.display = "block";
+		menu.style.borderLeft = "3px solid #19AA8D";
+		//alert("执行结束");
+	}
+	else
+	{
+		menu.style.height = "70px";
+		menu.style.borderLeft = "none";
 		sub_menu.style.display = "none";
 	}
 }
